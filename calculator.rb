712667ee1a9626b321cbe7215462e8176b56fc7e
Chris Tsui <chris.tsui5@gmail.com>
2016-07-29 08:29:19 -0700

    rubocop run

--- calculator.rb
+++ calculator.rb
@@ -4,15 +4,14 @@ def prompt(message)
 end
 
 def valid_number(test)
-  test.to_i != 0
-     
-end    
+  test.to_i.nonzero?()
+end
 prompt("Welcome to the calcuator program!")
 prompt("Please enter your name:")
 name = Kernel.gets().chomp()
 
 def choice(select)
-  case select 
+  case select
   when '1'
     'Adding'
   when '2'
@@ -21,10 +20,10 @@ def choice(select)
     'Mutiplying'
   when '4'
     'Dividing'
-  end            
-end  
+  end
+end
 
-loop do #name validation
+loop do # name validation
   if name.empty?()
     prompt("Please enter your name:")
     name = Kernel.gets().chomp()
@@ -32,9 +31,9 @@ loop do #name validation
     prompt("Hello #{name}!")
     break
   end
-end    
+end
 
-loop do #main loop
+loop do # main loop
 selection_prompt = <<-MES
 Please press:
 1. Addition
@@ -50,9 +49,10 @@ until (1..4).include?(selection.to_i())
   prompt("Please enter 1,2,3,4.")
   prompt(selection_prompt)
   selection = Kernel.gets().chomp()
-end 
+end
 
-number1,number2 = nil,nil 
+number1 = nil
+number2 = nil
 
 loop do
   prompt("What is the first number?")
@@ -62,9 +62,9 @@ loop do
   else
     prompt("Error! Please input a valid number.")
   end
-end    
+end
 
-loop do 
+loop do
   prompt("What is the second number?")
   number2 = Kernel.gets().chomp()
   if valid_number(number2)
@@ -83,7 +83,7 @@ results = case selection
             number1.to_i() * number2.to_i()
           when '4'
             number1.to_f() / number2.to_f()
-end  
+          end
 prompt("#{choice(selection)} #{number1} and #{number2} is #{results}")
 prompt("Do you want to run another calculation? Type y for yes")
 reply = Kernel.gets().chomp()
