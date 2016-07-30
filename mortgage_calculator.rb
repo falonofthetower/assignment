cf8a0e4acc15a7c7b65a4aeaf902ca650dcaeb4d
Chris Tsui <chris.tsui5@gmail.com>
2016-07-30 00:47:12 -0700

    better validation

--- mortgage_calculator.rb
+++ mortgage_calculator.rb
@@ -20,7 +20,7 @@ def number?(number)
 end
 
 def monthly_payment(loan, m_interest, months)
-  loan.to_f * (m_interest.to_f / (1 - (1 + m_interest.to_f)**-months.to_i))
+  loan.to_f * (m_interest.to_f / (1 - (1 + m_interest.to_f)**-months.to_f))
 end
 
 def monthly_interest_rate(yearly_interest)
@@ -28,7 +28,11 @@ def monthly_interest_rate(yearly_interest)
 end
 
 def number_months(number_years)
-  number_years.to_i * 12
+  number_years.to_f * 12
+end
+
+def error_message
+  prompt(MESSAGES['not_valid'])
 end
 
 prompt(MESSAGES['welcome'])
@@ -38,45 +42,36 @@ loop do
   principle = gets.chomp
 
   loop do
-    if number?(principle) && principle.to_f >= 0
-      break
-    else
-      prompt(MESSAGES['not_valid'])
-      principle = gets.chomp
-    end
+    break if number?(principle) && principle.to_f >= 0
+    error_message
+    principle = gets.chomp
   end
 
   prompt(MESSAGES['interest'])
   interest_rate = gets.chomp
 
   loop do
-    if number?(interest_rate)
-      break
-    else
-      prompt(MESSAGES['not_valid'])
-      interest_rate = gets.chomp
-    end
+    break if number?(interest_rate) && interest_rate.to_f > 0
   end
 
   prompt(MESSAGES['duration'])
   years = gets.chomp
 
   loop do
-    if years.to_i > 0 && integer?(years)
-      break
-    else
-      prompt(MESSAGES['not_valid'])
-      prompt(MESSAGES['duration'])
-      years = gets.chomp
-    end
+    break if years.to_i > 0 && number?(years)
+    break if years.to_f > 0 && number?(years)
+    error_message
+    prompt(MESSAGES['duration'])
+    years = gets.chomp
   end
 
   monthly_interest = monthly_interest_rate(interest_rate) / 100
   months = number_months(years)
-  prompt("Principle:  $#{principle}")
-  prompt("Interest Rate:  #{interest_rate}%")
-  prompt("Monthly payment:\
-  $#{monthly_payment(principle, monthly_interest, months).round(2)}")
+  prompt("Principle: $#{principle}")
+  prompt("Interest Rate: #{interest_rate}%")
+  prompt("Term(months): #{months.round(2)}")
+  prompt("Monthly payment: "\
+         "$#{monthly_payment(principle, monthly_interest, months).round(2)}")
   prompt(MESSAGES['run_again'])
   break unless gets.chomp.downcase.start_with?('y')
 end
